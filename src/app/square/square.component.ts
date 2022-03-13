8d151a9bcb20cebec12254e86c90aea4f1956df5
Andras Varga <vargaae@hotmail.com>
2022-03-13 20:17:02 +0100

    set styling: @nebular/theme

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -11,7 +11,7 @@ import { Component, Input } from '@angular/core';
       {{ value }}
     </button>
   `,
-  styles: [],
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'],
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O';
